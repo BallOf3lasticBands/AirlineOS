8e808a152b027a67e0b058599aef2a91a0e13635
Avery <69469676+BallOf3lasticBands@users.noreply.github.com>
2026-08-05 15:29:18 +0200

    Update includes & auth
    Updated to work with the singular points and auth system

--- public/add.php
+++ public/add.php
@@ -1,34 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Add data - Simple CRUD Using PHP, MySQL and Bootstrap</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container mt-4">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="page-header mb-4">
-                    <h2>Add a new customer</h2>
-                </div>
-                <form action="../functions/add_process.php" method="post">
-                    <div class="form-group">
-                        <label>First name</label>
-                        <input type="text" name="firstname" class="form-control" required="">
-                    </div>
-                    <div class="form-group">
-                        <label>Last name</label>
-                        <input type="text" name="lastname" class="form-control" required="">
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input type="email" name="email" class="form-control" required="">
-                    </div>
-                    <input type="submit" class="btn btn-primary" name="submit" value="save">
-                </form>
-            </div>
+<?php
+require_once __DIR__ . '/../includes/config.php';
+$require_login = true; // protect this page for logged-in users
+require_once __DIR__ . '/../includes/auth.php';
+
+$page_title = 'Add Customer';
+require_once __DIR__ . '/../includes/header.php';
+?>
+<div class="row">
+    <div class="col-md-8">
+        <div class="page-header mb-4">
+            <h2>Add a new customer</h2>
         </div>
+        <form action="/functions/add_process.php" method="post">
+            <div class="form-group">
+                <label>First name</label>
+                <input type="text" name="firstname" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label>Last name</label>
+                <input type="text" name="lastname" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" name="email" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
     </div>
-</body>
-</html>
+</div>
+<?php require_once __DIR__ . '/../includes/footer.php';
--- public/edit.php
+++ public/edit.php
@@ -1,44 +1,38 @@
-<?php 
-include 'database_conn.php';
+<?php
+require_once __DIR__ . '/../includes/config.php';
+$require_login = true; // protect this page for logged-in users
+require_once __DIR__ . '/../includes/auth.php';
 
-$customer_id = $_GET['customer_id'];
+$customer_id = isset($_GET['customer_id']) ? (int) $_GET['customer_id'] : 0;
 
-$query = "SELECT * FROM customers WHERE customer_id='{$customer_id}'";
+$query = "SELECT * FROM customers WHERE customer_id = {$customer_id}";
 $query_res = mysqli_query($db_connect, $query);
-$customer = mysqli_fetch_assoc($query_res); ?>
+$customer = mysqli_fetch_assoc($query_res);
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Edit customer - Simple CRUD Using PHP, MySQL and Bootstrap</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container mt-4">
+$page_title = 'Edit Customer';
+require_once __DIR__ . '/../includes/header.php';
+?>
+<div class="row">
+    <div class="col-md-8">
         <div class="page-header mb-4">
             <h2>Edit customer</h2>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <form action="/functions/edit_process.php" method="POST">
-                    <input type="hidden" name="customer_id" value="<?php echo $_GET['customer_id']; ?>" class="form-control" required="">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">First name</label>
-                        <input type="text" name="firstname" class="form-control" value="<?php echo $customer['firstname']; ?>" required="">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Last name</label>
-                        <input type="text" name="lastname" class="form-control" value="<?php echo $customer['lastname']; ?>" required="">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email</label>
-                        <input type="email" name="email" class="form-control" value="<?php echo $customer['email']; ?>" required="">
-                    </div>
-                    <button type="submit" class="btn btn-primary" value="submit">Submit</button>
-                </form>
+        <form action="/functions/edit_process.php" method="POST">
+            <input type="hidden" name="customer_id" value="<?= htmlspecialchars($customer_id) ?>" class="form-control" required>
+            <div class="form-group">
+                <label for="firstname">First name</label>
+                <input id="firstname" type="text" name="firstname" class="form-control" value="<?= htmlspecialchars($customer['firstname'] ?? '') ?>" required>
             </div>
-        </div>
+            <div class="form-group">
+                <label for="lastname">Last name</label>
+                <input id="lastname" type="text" name="lastname" class="form-control" value="<?= htmlspecialchars($customer['lastname'] ?? '') ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" class="form-control" value="<?= htmlspecialchars($customer['email'] ?? '') ?>" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-</body>
-</html>
+</div>
+<?php require_once __DIR__ . '/../includes/footer.php';
